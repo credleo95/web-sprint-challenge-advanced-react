7befb593b5366c1d2120c8b8f37e7f20873391c7
Omarius Credle <omariuscredle@omariuss-air.home>
2021-05-16 23:08:36 -0400

    cleaned up code from useLocalStorage. Added ternary.

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,1 +1,18 @@
-import { useState } from 'react' ; 
+import { useState } from 'react' ;
+
+
+const useLocalStorage = (key, initialValues) => {
+    const [storedValues, setStoredValues] = useState(() => {
+        const item = localStorage.getItem(key); 
+        return item ? JSON.parse(item) : initialValues;
+    }); 
+
+    const setValues = values => {
+        setStoredValues(values);
+        localStorage.setItem(key, JSON.stringify(values))
+    }
+
+    return[storedValues, setValues]
+}
+
+export default useLocalStorage; 
