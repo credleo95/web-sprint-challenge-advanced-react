374d5628c1284b865ac499f0b75e5ae6521f33f4
Omarius Credle <omariuscredle@omariuss-air.home>
2021-05-16 23:29:27 -0400

    created useForm hook and added to checkoutForm. Working through bug

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,8 +1,21 @@
 import {useState } from 'react'; 
+import useLocalStorage from './useLocalStorage'; 
+
+const useForm = (key, initialValues, callback ) => {
+    const [stateValues, setStateValues] = useLocalStorage(initialValues);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
 
-const useForm = () => {
+    const handleChange = event => {
+        setStateValues({...stateValues,[event.target.name]: event.target.value})
+    }
 
+    const handleSubmit = event => {
+        event ? event.preventDefault() : callback(stateValues)
+        setShowSuccessMessage(true)
+    }
+
+return [stateValues, handleChange, handleSubmit, showSuccessMessage]
 }
 
 export default useForm 
