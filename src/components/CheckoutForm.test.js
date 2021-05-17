e19aa9a97de4e83a93d317ff6380502bf9874a67
Omarius Credle <omariuscredle@omariuss-air.home>
2021-05-16 23:56:20 -0400

    Wrote required tests.All are passing. Met MVP.

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />); 
+    const header = screen.getByText(/checkout form/i); 
+    expect(header).toBeVisible()
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+render(<CheckoutForm />);
+const button = screen.getByText("Checkout"); 
+userEvent.click(button)
+const success = screen.getByTestId("successMessage");
+expect(success).toBeVisible()
+
+});
